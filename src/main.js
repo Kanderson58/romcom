6523d30de134c7a25a4c7592e2b6c1e69b0723ff
Kara Anderson <kanderson8585@gmail.com>
2022-12-08 17:14:39 -0700

    feat: Shorten variables for randomizing function

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ var saveCoverButton = document.querySelector(".save-cover-button")
 var viewSavedCoverButton = document.querySelector(".view-saved-button")
 var makeNewCoverButton = document.querySelector(".make-new-button")
 
-
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -31,15 +30,10 @@ makeNewCoverButton.addEventListener("click", showForm)
 // Create your event handlers and other functions here 👇
 
 function getRandomCover(){
-  var randomCoversIndex = getRandomIndex(covers);
-  var randomTitlesIndex = getRandomIndex(titles);
-  var randomDescriptorsIndex1 = getRandomIndex(descriptors);
-  var randomDescriptorsIndex2 = getRandomIndex(descriptors);
-
-  var randomCoverImage = covers[randomCoversIndex]
-  var randomCoverTitle = titles[randomTitlesIndex]
-  var randomCoverDescriptor1 = descriptors[randomDescriptorsIndex1]
-  var randomCoverDescriptor2 = descriptors[randomDescriptorsIndex2]
+  var randomCoverImage = covers[getRandomIndex(covers)];
+  var randomCoverTitle = titles[getRandomIndex(titles)];
+  var randomCoverDescriptor1 = descriptors[getRandomIndex(descriptors)];
+  var randomCoverDescriptor2 = descriptors[getRandomIndex(descriptors)];
 
   coverLocation.src = randomCoverImage
   titleLocation.innerText = randomCoverTitle
@@ -50,7 +44,6 @@ function getRandomCover(){
 }
 
 function showForm() {
-  console.log("howwwdy")
   viewFormPage.classList.remove("hidden")
   viewHome.classList.add("hidden")
   randomCoverButton.classList.add("hidden")
@@ -63,3 +56,9 @@ function getRandomIndex(array) {
 }
 
 
+// Iteration 1:
+// view saved cover button actions: show saved covers section, hide show new random cover button, hide save cover button, make home button visible
+// home button actions: show home section, hide home button, show new random cover button, show save cover button
+
+// Iteration2:
+// create input boxes for form view
